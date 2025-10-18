e6d7219ddff59130943e2a02012202ac1dec0218
tuturdhy <tuturdhy@gmail.com>
2025-10-18 01:44:40 +0100

    modification concernant weather

--- users/views.py
+++ users/views.py
@@ -3,7 +3,19 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import UserRegistrationSerializer, UserProfileSerializer
+# Ajoutez en haut du fichier users/views.py
+from rest_framework import serializers
 
+class EmptySerializer(serializers.Serializer):
+    """Serializer vide pour les views sans données"""
+    pass
+
+class VerifyEmailView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = EmptySerializer  # 🆕 UTILISEZ CE SERIALIZER
+    
+    def post(self, request):
+        return Response({"message": "Email verification will be implemented soon"})
 User = get_user_model()
 
 class UserRegistrationView(generics.CreateAPIView):
